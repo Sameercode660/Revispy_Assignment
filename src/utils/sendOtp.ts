84c1ce639656723cc1cecf2fb293b1bcb78edcb7
Mohd Sameer <mesh789736@gmail.com>
2025-03-03 17:35:10 +0530

    postintall added

--- src/utils/sendOtp.ts
+++ src/utils/sendOtp.ts
@@ -2,12 +2,10 @@ import nodemailer from "nodemailer";
 
 // Configure your SMTP settings
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "Gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Not your normal password
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
